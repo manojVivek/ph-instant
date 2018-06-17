f88a03a22ba4888426833c31f805c7954c92410e
Manoj Vivek <p.manoj.vivek@gmail.com>
2018-06-17 08:38:29 +0530

    Opening the producthunt page on click of the notification

--- client/src/notify.js
+++ client/src/notify.js
@@ -1,14 +1,23 @@
 'use strict';
 
+chrome.notifications.onClicked.addListener(notificationId => {
+  chrome.tabs.create({url: notificationId}, tab => {
+    chrome.windows.update(tab.windowId, {focused: true});
+  });
+  chrome.notifications.clear(notificationId);
+});
+
 export function sendNotification(post) {
   console.log('Sending notification', post.id);
   chrome.notifications.create(
-    post.id.toString(),
+    post.url,
     {
       type: 'basic',
       iconUrl: '../static/ph_instant_logo.png',
       title: "New PH: " + post.name,
       message: post.tagline,
+      requireInteraction: true,
+      priority: 2,
     },
     data => console.log("Callback data", data)
   );
